eb4faac32557a08ddc18dc41d6d611ed4702fdb7
55tommy <tomisawakana@gmail.com>
2022-10-12 14:08:16 +0900

    dependent: :destroyの追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.create(comment_params)
-    
+     
     if @comment.save
        redirect_to prototype_path(@comment.prototype)
     else
